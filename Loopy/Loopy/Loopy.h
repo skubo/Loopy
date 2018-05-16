719e630cae2f1b829835fbc50304f58e604b4977
skubo <skubo08@gmail.com>
2018-05-16 13:59:42 +0200

    framwork project trunk for both device and sim

---
+++ Loopy/Loopy/Loopy.h
@@ -0,0 +1,19 @@
+//
+//  Loopy.h
+//  Loopy
+//
+//  Created by txitax on 16.05.18.
+//  Copyright © 2018 Christian Garbers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Loopy.
+FOUNDATION_EXPORT double LoopyVersionNumber;
+
+//! Project version string for Loopy.
+FOUNDATION_EXPORT const unsigned char LoopyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loopy/PublicHeader.h>
+
+
